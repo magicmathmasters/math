cd0ca66a6048d5828da570b631ed1fd127a81eb1
MagicMathMaster <magicmathmaster23@gmail.com>
2026-02-13 14:12:18 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 
   document.querySelectorAll('a[data-nav]').forEach(a => {
     const href = (a.getAttribute("href") || "").toLowerCase();
-    if (href === file) a.setAttribute("aria-current", "page");
+    const hrefFile = href.split("/").pop().split("#")[0].split("?")[0];
+
+    if (hrefFile === file) a.setAttribute("aria-current", "page");
+    else a.removeAttribute("aria-current");
   });
 })();
