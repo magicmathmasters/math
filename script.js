30168099199d7b4a8c51c91542edc1d2613bc115
MagicMathMaster <magicmathmaster23@gmail.com>
2026-03-01 20:56:34 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -10,27 +10,78 @@
     else a.removeAttribute("aria-current");
   });
 
-  const languageSelect = document.getElementById("language-select");
+  const langBtn = document.getElementById("langBtn");
+  const langDropdown = document.getElementById("langDropdown");
+  const currentLangFlag = document.getElementById("currentLangFlag");
+  const currentLangText = document.getElementById("currentLangText");
+  const langOptions = document.querySelectorAll(".lang-option");
 
-  if (languageSelect) {
+  if (langBtn && langDropdown && currentLangFlag && currentLangText && langOptions.length) {
     const path = window.location.pathname.toLowerCase();
+    const fileName = (path.split("/").pop() || "index.html").toLowerCase();
 
-    if (path.indexOf("/vi/") === 0) {
-      languageSelect.value = "/vi/index.html";
-    } else if (path.indexOf("/zh/") === 0) {
-      languageSelect.value = "/zh/index.html";
-    } else if (path.indexOf("/hi/") === 0) {
-      languageSelect.value = "/hi/index.html";
-    } else if (path.indexOf("/es/") === 0) {
-      languageSelect.value = "/es/index.html";
-    } else {
-      languageSelect.value = "/index.html";
-    }
+    let currentCode = "en";
 
-    languageSelect.addEventListener("change", function () {
-      if (this.value) {
-        window.location.href = this.value;
+    if (path.indexOf("/vi/") !== -1) currentCode = "vi";
+    else if (path.indexOf("/zh/") !== -1) currentCode = "zh";
+    else if (path.indexOf("/hi/") !== -1) currentCode = "hi";
+    else if (path.indexOf("/es/") !== -1) currentCode = "es";
+
+    langOptions.forEach(option => {
+      const code = option.getAttribute("data-lang");
+      const flag = option.getAttribute("data-flag");
+      const label = option.getAttribute("data-label");
+
+      if (code === currentCode) {
+        currentLangFlag.textContent = flag;
+        currentLangText.textContent = label;
+        option.classList.add("active");
+      } else {
+        option.classList.remove("active");
       }
+
+      option.addEventListener("click", function () {
+        let target = "index.html";
+
+        if (code === "vi") target = "vi/" + fileName;
+        else if (code === "zh") target = "zh/" + fileName;
+        else if (code === "hi") target = "hi/" + fileName;
+        else if (code === "es") target = "es/" + fileName;
+
+        // If currently inside a language folder, go up one level first
+        if (currentCode !== "en") {
+          if (code === "en") {
+            target = "../" + fileName;
+          } else {
+            target = "../" + target;
+          }
+        }
+
+        window.location.href = target;
+      });
+    });
+
+    langBtn.addEventListener("click", function (e) {
+      e.stopPropagation();
+
+      const isOpen = !langDropdown.hasAttribute("hidden");
+
+      if (isOpen) {
+        langDropdown.setAttribute("hidden", "");
+        langBtn.setAttribute("aria-expanded", "false");
+      } else {
+        langDropdown.removeAttribute("hidden");
+        langBtn.setAttribute("aria-expanded", "true");
+      }
+    });
+
+    langDropdown.addEventListener("click", function (e) {
+      e.stopPropagation();
+    });
+
+    document.addEventListener("click", function () {
+      langDropdown.setAttribute("hidden", "");
+      langBtn.setAttribute("aria-expanded", "false");
     });
   }
 })();
