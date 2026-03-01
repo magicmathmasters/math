92af375fac756a2c025a1a303a9e412f9798d17c
MagicMathMaster <magicmathmaster23@gmail.com>
2026-03-01 19:57:47 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -9,4 +9,28 @@
     if (hrefFile === file) a.setAttribute("aria-current", "page");
     else a.removeAttribute("aria-current");
   });
+
+  const languageSelect = document.getElementById("language-select");
+
+  if (languageSelect) {
+    const path = window.location.pathname.toLowerCase();
+
+    if (path.indexOf("/vi/") === 0) {
+      languageSelect.value = "/vi/index.html";
+    } else if (path.indexOf("/zh/") === 0) {
+      languageSelect.value = "/zh/index.html";
+    } else if (path.indexOf("/hi/") === 0) {
+      languageSelect.value = "/hi/index.html";
+    } else if (path.indexOf("/es/") === 0) {
+      languageSelect.value = "/es/index.html";
+    } else {
+      languageSelect.value = "/index.html";
+    }
+
+    languageSelect.addEventListener("change", function () {
+      if (this.value) {
+        window.location.href = this.value;
+      }
+    });
+  }
 })();
